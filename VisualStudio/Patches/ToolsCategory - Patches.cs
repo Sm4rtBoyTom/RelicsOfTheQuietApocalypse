5f97e389a43e62a220ee36af99a179d065546b3c
Sm4rtBoyTom <155878804+Sm4rtBoyTom@users.noreply.github.com>
2025-07-24 17:31:01 +0200

    Add files via upload

---
+++ VisualStudio/Patches/ToolsCategory - Patches.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using Il2Cpp;
+using UnityEngine;
+using Il2CppSystem.Collections;
+using MelonLoader;
+using UnityEngine.UIElements;
+
+namespace RetroTexturesMod
+{
+    [HarmonyPatch(typeof(GearItem), nameof(GearItem.Awake))]
+public static class PrybarPatches
+{
+    public static void Postfix(GearItem __instance)
+    {
+            if (Settings.instance.Prybar)
+            {
+                if (!__instance.name.StartsWith("GEAR_Prybar")) return;
+
+
+        Transform root = __instance.transform;
+
+        Transform lod1 = root.Find("OBJ_PryBar_LOD1");
+
+
+        if (lod1 != null)
+            lod1.gameObject.SetActive(false);
+            }
+        }
+    }
+}
